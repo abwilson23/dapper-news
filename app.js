b128b24d2a84a19544417136fedc4b7620a62b07
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-08-07 15:28:32 -0700

    forgot to add these

--- app.js
+++ app.js
@@ -1,77 +1,60 @@
-var app = angular.module('dapperNews', ['ui.router']);
-app.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		$stateProvider
-		  .state('home', {
-		    url: '/home',
-			templateUrl: '/home.html',
-			controller: 'MainCtrl'
-		  })
-		  .state('posts', {
-			url: '/posts/{id}',
-			templateUrl: '/posts.html',
-			controller: 'PostCtrl'
-		  });
-		$urlRouterProvider.otherwise('home');
-}]);
-app.factory('posts', [function() {
-	var o = {
-		posts: []
-	};
-	return o;
-}]);
-app.controller('MainCtrl', [
-	'$scope',
-	'posts',
-	function($scope, posts) {
-		$scope.posts = posts.posts;
-		$scope.addPost = function() {
-		if ($scope.title === '') {return; }
-		$scope.posts.push({
-			title: $scope.title, 
-			link: $scope.link, 
-			upvotes: 0,
-			comments: [
-				{author: 'Joe', body: 'Cool post!', upvotes: 0},
-				{author: 'Bob', body: 'Good idea, but wrong, nerd', upvotes: 0}
-			]});
-		$scope.title = '';
-		$scope.link = '';
-		};
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-		$scope.incrementUpvotes =  function(post) {
-		post.upvotes += 1;
-		};
-}]);
-app.controller('PostCtrl', [
-	'$scope',
-	'$stateParams',
-	'posts',
-	function($scope, $stateParams, posts) {
-		$scope.post = posts.posts[$stateParams.id];
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-		$scope.addComment = function() {
-			if ($scope.body === '') {return; }
-			$scope.post.comments.push({
-				body: $scope.body, 
-				author: 'user', 
-				upvotes: 0,
-			});
-			$scope.body = '';
-		};
+var app = express();
 
-		$scope.incrementUpvotes =  function(comment) {
-		comment.upvotes += 1;
-		};
-}]);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
-
-
-
-
-
-
-
+module.exports = app;
