0d479366a6b9e80e6d21ccb406d0cfb5f376b73c
Andrew Wilson <tzinquaw.bp@gmail.com>
2015-08-06 18:56:40 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,77 @@
+var app = angular.module('dapperNews', ['ui.router']);
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		  .state('home', {
+		    url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		  })
+		  .state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostCtrl'
+		  });
+		$urlRouterProvider.otherwise('home');
+}]);
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+app.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts) {
+		$scope.posts = posts.posts;
+		$scope.addPost = function() {
+		if ($scope.title === '') {return; }
+		$scope.posts.push({
+			title: $scope.title, 
+			link: $scope.link, 
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Good idea, but wrong, nerd', upvotes: 0}
+			]});
+		$scope.title = '';
+		$scope.link = '';
+		};
+
+		$scope.incrementUpvotes =  function(post) {
+		post.upvotes += 1;
+		};
+}]);
+app.controller('PostCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function() {
+			if ($scope.body === '') {return; }
+			$scope.post.comments.push({
+				body: $scope.body, 
+				author: 'user', 
+				upvotes: 0,
+			});
+			$scope.body = '';
+		};
+
+		$scope.incrementUpvotes =  function(comment) {
+		comment.upvotes += 1;
+		};
+}]);
+
+
+
+
+
+
+
+
+
